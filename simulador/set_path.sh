a9e7fb8b9f2da95c3bb3403472a3796091e38887
Marcelo <martimiano.marcelo@gmail.com>
2016-10-26 16:38:50 -0200

    primeiro teste

--- simulador/set_path.sh
+++
@@ -1,5 +0,0 @@
-export LD_LIBRARY_PATH=/home/specg12-1/mc404/simulador/deps/arm-eabi-deps:/home/specg12-1/mc404/simulador/deps/systemc
-export PATH=$PATH:/home/specg12-1/mc404/simulador/deps/archc
-export PATH=$PATH:/home/specg12-1/mc404/simulador/bin
-export PATH=$PATH:/home/specg12-1/mc404/simulador/arm-eabi-4.4.3/bin
-export CROSS_COMPILE=arm-eabi-
