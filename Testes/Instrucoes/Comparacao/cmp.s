b8692aaa3fd590e569551e7ad01b710b0c08665e
Marcelo Martimiano Junior <ra156531@margarida.lab.ic.unicamp.br>
2016-11-30 17:36:40 -0200

    mais testes

--- Testes/Instrucoes/Comparacao/cmp.s
+++ Testes/Instrucoes/Comparacao/cmp.s
@@ -3,11 +3,14 @@
     .globl _start   @ torna o símbolo main visivel fora do arquivo
 
 _start:
-	mov r0, #0b101
-	loop:
-	subs r0, r0, #1
-	bcs loop
-		
+	mov r0, #15
+	cmp r0, #15
+	moveq r2, #12	@ move se for igual	
+	moveq r3, #123	@ move se for igual
+
+	cmp r0, #10
+	movne r4, #13	@ move se for diferente
+	
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@	
     mov r7, #1      @ carrega o valor 1 em r7, indicando a escolha da  
     svc 0x0         
